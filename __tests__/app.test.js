37ac05a550276b374390e0ee0e04a2b1ab9eaad5
Anddy <alonzoa2015@gmail.com>
2022-06-06 17:33:30 -0700

    first test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,18 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const { zodiacs } = require('../data/zodiacs');
+
 describe('fs-zodic-api routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('/zodiacs responds with 200 and an array of zodiacs', async () => {
+    const res = await request(app).get('/zodiacs');
+    const expected = zodiacs;
+
+    expect(res.body).toEqual(expected);
   });
 
   afterAll(() => {
