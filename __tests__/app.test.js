c2a6af3d957c300f0f4529d044233087f72c62d6
Anddy <alonzoa2015@gmail.com>
2022-06-06 17:53:58 -0700

    refactored first route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,7 +12,68 @@ describe('fs-zodic-api routes', () => {
 
   it('/zodiacs responds with an array of zodiacs', async () => {
     const res = await request(app).get('/api/zodiacs');
-    const expected = zodiacs;
+    const expected = [
+      {
+        'id': '1',
+        'name': 'aquarius',
+        'symbol': 'Water Bearer'
+      },
+      {
+        'id': '2',
+        'name': 'aries',
+        'symbol': 'Ram'
+      },
+      {
+        'id': '3',
+        'name': 'cancer',
+        'symbol': 'Crab'
+      },
+      {
+        'id': '4',
+        'name': 'capricorn',
+        'symbol': 'Goat'
+      },
+      {
+        'id': '5',
+        'name': 'gemini',
+        'symbol': 'Twins'
+      },
+      {
+        'id': '6',
+        'name': 'leo',
+        'symbol': 'Lion'
+      },
+      {
+        'id': '7',
+        'name': 'libra',
+        'symbol': 'Balance'
+      },
+      {
+        'id': '8',
+        'name': 'pisces',
+        'symbol': 'Fish'
+      },
+      {
+        'id': '9',
+        'name': 'sagittarius',
+        'symbol': 'Archer'
+      },
+      {
+        'id': '10',
+        'name': 'scorpio',
+        'symbol': 'Scorpion'
+      },
+      {
+        'id': '11',
+        'name': 'taurus',
+        'symbol': 'Bull'
+      },
+      {
+        'id': '12',
+        'name': 'virgo',
+        'symbol': 'Virgin'
+      }
+    ];
     expect(res.body).toEqual(expected);
   });
 
